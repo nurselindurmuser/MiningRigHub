5b74a753c6502eee7ed4bc5cb3d4202833176023
Nurselin Durmuşer <nurselin.durmuser@gmail.com>
2018-11-20 17:36:25 +0300

    v1

---
+++ js/site.js
@@ -0,0 +1,11 @@
+ // Gallery carousel (uses the Owl Carousel library)
+ $(".gallery-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    center:true,
+    responsive: { 0: { items: 1 }, 768: { items: 3 }, 992: { items: 4 }, 1200: {items: 5}
+    }
+  });
+
+  
